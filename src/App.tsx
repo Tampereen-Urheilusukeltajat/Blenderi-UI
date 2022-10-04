e31fb44f8168b17872435a4b6b6b56bbd1d96565
Jaakko Kääriäinen <jaakkoakaariainen@gmail.com>
2022-10-04 21:05:27 +0300

    UI: Rekisteröitymisen pohja (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { Routes, Route } from 'react-router-dom';
+import Home from './views/Home';
+import SignUp from './views/SignUp';
 
 function App() {
   return (
     <div>
-      Hello blenderilasku UI!
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="register" element={<SignUp />} />
+        </Routes>
+      </Container>
     </div>
   );
 }
---
+++ src/components/SignUpForm.tsx
@@ -0,0 +1,145 @@
+import React, { useCallback, useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+const SignUpFormHeader = () => {
+  return <h3>Luo uusi käyttäjä</h3>;
+};
+
+const SignUpForm = () => {
+  const [values, setValues] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  });
+
+  const handleFirstNameInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newValues = { ...values };
+      newValues['firstName'] = e.target.value;
+      setValues(newValues);
+    },
+    [values]
+  );
+
+  const handleLastNameInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newValues = { ...values };
+      newValues['lastName'] = e.target.value;
+      setValues(newValues);
+    },
+    [values]
+  );
+
+  const handleUserNameInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newValues = { ...values };
+      newValues['username'] = e.target.value;
+      setValues(newValues);
+    },
+    [values]
+  );
+
+  const handlePasswordInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newValues = { ...values };
+      newValues['password'] = e.target.value;
+      setValues(newValues);
+    },
+    [values]
+  );
+
+  const handlePasswordConfirmInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newValues = { ...values };
+      newValues['passwordConfirm'] = e.target.value;
+      setValues(newValues);
+    },
+    [values]
+  );
+
+  return (
+    <div>
+      <SignUpFormHeader />
+      <Form>
+        <Form.Group className="mb-3" controlId="formFirstName">
+          <Form.Label>Etunimi</Form.Label>
+          <Form.Control
+            aria-label="Etunimi"
+            type="text"
+            placeholder=""
+            value={values.firstName}
+            onChange={handleFirstNameInputChange}
+            required
+          ></Form.Control>
+          <Form.Control.Feedback type="invalid">
+            Anna etunimi.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formLastName">
+          <Form.Label>Sukunimi</Form.Label>
+          <Form.Control
+            aria-label="Sukunimi"
+            type="text"
+            placeholder=""
+            value={values.lastName}
+            onChange={handleLastNameInputChange}
+            required
+          ></Form.Control>
+          <Form.Control.Feedback type="invalid">
+            Anna sukunimi.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formUserId">
+          <Form.Label>Käyttäjätunnus</Form.Label>
+          <Form.Control
+            aria-label="Käyttäjätunnus"
+            type="text"
+            placeholder=""
+            value={values.username}
+            onChange={handleUserNameInputChange}
+            required
+          ></Form.Control>
+          <Form.Control.Feedback type="invalid">
+            Anna käyttäjätunnus.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPassword">
+          <Form.Label>Salasana</Form.Label>
+          <Form.Control
+            aria-label="Salasana"
+            type="password"
+            placeholder=""
+            value={values.password}
+            onChange={handlePasswordInputChange}
+            required
+          ></Form.Control>
+          <Form.Control.Feedback type="invalid">
+            Anna salasana.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formConfirmPassword">
+          <Form.Label>Salasana uudelleen</Form.Label>
+          <Form.Control
+            aria-label="Salasana uudelleen"
+            type="password"
+            placeholder=""
+            value={values.passwordConfirm}
+            onChange={handlePasswordConfirmInputChange}
+            required
+          ></Form.Control>
+          <Form.Control.Feedback type="invalid">
+            Toista salasana uudelleen.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Button aria-label="Luo käyttäjä" variant="primary" type="submit">
+          Luo käyttäjä
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default SignUpForm;
