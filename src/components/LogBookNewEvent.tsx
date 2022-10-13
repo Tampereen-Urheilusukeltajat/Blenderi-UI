c8d72dc992f603a18cfe46bea9df1e25caf9bfbc
Jaakko Kääriäinen <jaakkoakaariainen@gmail.com>
2022-10-13 19:58:21 +0300

    Pilkottu täyttörivi ja labelit omiksi komponenteiksi. Formik lisätty.

--- src/components/LogBookNewEvent.tsx
+++ src/components/LogBookNewEvent.tsx
@@ -1,121 +1,120 @@
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import { Col, Form, InputGroup, Row } from 'react-bootstrap';
+import { Formik } from 'formik';
+
+const LogBookFieldRow = () => {
+  return (
+    <Row>
+      <Form.Group as={Col} controlId="pullo">
+        <Form.Label>Pullo</Form.Label>
+      </Form.Group>
+      <Form.Group as={Col} controlId="kaasu">
+        <Form.Label>Kaasu</Form.Label>
+      </Form.Group>
+      <Form.Group as={Col} controlId="kaasu2">
+        <Form.Label>Kaasu2 (vapaaehtoinen)</Form.Label>
+      </Form.Group>
+      <Form.Group as={Col} controlId="kaasu3">
+        <Form.Label>Kaasu3 (vapaaehtoinen)</Form.Label>
+      </Form.Group>
+      <Form.Group as={Col} controlId="tayttopaine">
+        <Form.Label>Täyttöpaine</Form.Label>
+      </Form.Group>
+      <Form.Group as={Col} controlId="lisatiedot">
+        <Form.Label>Lisätiedot</Form.Label>
+      </Form.Group>
+      <Form.Group as={Col} controlId="hinta">
+        <Form.Label>Hinta</Form.Label>
+      </Form.Group>
+      <Form.Group as={Col} controlId="poista"></Form.Group>
+    </Row>
+  );
+};
+
+const LogBookRow = () => {
+  return (
+    <Row className="mt-2">
+      <Form.Group as={Col} controlId="pullo">
+        <Form.Select defaultValue="Valitse pullo">
+          <option>Valitse pullo</option>
+          <option>...</option>
+        </Form.Select>
+      </Form.Group>
+      <Form.Group as={Col} controlId="kaasu">
+        <Form.Select defaultValue="Valitse kaasu">
+          <option>Valitse kaasu</option>
+          <option>...</option>
+        </Form.Select>
+      </Form.Group>
+      <Form.Group as={Col} controlId="kaasu2">
+        <Form.Select defaultValue="Valitse kaasu">
+          <option>Valitse kaasu</option>
+          <option>...</option>
+        </Form.Select>
+      </Form.Group>
+      <Form.Group as={Col} controlId="kaasu3">
+        <Form.Select defaultValue="Valitse kaasu">
+          <option>Valitse kaasu</option>
+          <option>...</option>
+        </Form.Select>
+      </Form.Group>
+      <InputGroup as={Col} controlId="tayttopaine">
+        <Form.Control type="number" defaultValue={0} />
+        <InputGroup.Text>bar</InputGroup.Text>
+      </InputGroup>
+      <Form.Group as={Col} controlId="lisatiedot">
+        <Form.Control placeholder="" />
+      </Form.Group>
+      <Form.Group as={Col} controlId="hinta">
+        <p>0,00 €</p>
+      </Form.Group>
+      <Form.Group as={Col} controlId="poista">
+        <Button className="removeRowButton">Poista rivi</Button>
+      </Form.Group>
+    </Row>
+  );
+};
 
 const LogBookNewEvent = () => {
   return (
     <div>
       <h3 className="mb-5">Uusi täyttötapahtuma</h3>
-      <Form>
-        <Row>
-          <Form.Group as={Col} controlId="pullo">
-            <Form.Label>Pullo</Form.Label>
-          </Form.Group>
-          <Form.Group as={Col} controlId="kaasu">
-            <Form.Label>Kaasu</Form.Label>
-          </Form.Group>
-          <Form.Group as={Col} controlId="kaasu2">
-            <Form.Label>Kaasu2 (vapaaehtoinen)</Form.Label>
-          </Form.Group>
-          <Form.Group as={Col} controlId="kaasu3">
-            <Form.Label>Kaasu3 (vapaaehtoinen)</Form.Label>
-          </Form.Group>
-          <Form.Group as={Col} controlId="tayttopaine">
-            <Form.Label>Täyttöpaine</Form.Label>
-          </Form.Group>
-          <Form.Group as={Col} controlId="lisatiedot">
-            <Form.Label>Lisätiedot</Form.Label>
-          </Form.Group>
-          <Form.Group as={Col} controlId="hinta">
-            <Form.Label>Hinta</Form.Label>
-          </Form.Group>
-          <Form.Group as={Col} controlId="poista"></Form.Group>
-        </Row>
-        <Row className="mt-2">
-          <Form.Group as={Col} controlId="pullo">
-            <Form.Select defaultValue="Valitse pullo">
-              <option>Valitse pullo</option>
-              <option>...</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group as={Col} controlId="kaasu">
-            <Form.Select defaultValue="Valitse kaasu">
-              <option>Valitse kaasu</option>
-              <option>...</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group as={Col} controlId="kaasu2">
-            <Form.Select defaultValue="Valitse kaasu">
-              <option>Valitse kaasu</option>
-              <option>...</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group as={Col} controlId="kaasu3">
-            <Form.Select defaultValue="Valitse kaasu">
-              <option>Valitse kaasu</option>
-              <option>...</option>
-            </Form.Select>
-          </Form.Group>
-          <InputGroup as={Col} controlId="tayttopaine">
-            <Form.Control type="number" defaultValue={0} />
-            <InputGroup.Text>bar</InputGroup.Text>
-          </InputGroup>
-          <Form.Group as={Col} controlId="lisatiedot">
-            <Form.Control placeholder="" />
-          </Form.Group>
-          <Form.Group as={Col} controlId="hinta">
-            <p>0,00 €</p>
-          </Form.Group>
-          <Form.Group as={Col} controlId="poista">
-            <Button className="removeRowButton">Poista rivi</Button>
-          </Form.Group>
-        </Row>
-        <Row className="mt-2">
-          <Form.Group as={Col} controlId="pullo">
-            <Form.Select defaultValue="Valitse pullo">
-              <option>Valitse pullo</option>
-              <option>...</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group as={Col} controlId="kaasu">
-            <Form.Select defaultValue="Valitse kaasu">
-              <option>Valitse kaasu</option>
-              <option>...</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group as={Col} controlId="kaasu2">
-            <Form.Select defaultValue="Valitse kaasu">
-              <option>Valitse kaasu</option>
-              <option>...</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group as={Col} controlId="kaasu3">
-            <Form.Select defaultValue="Valitse kaasu">
-              <option>Valitse kaasu</option>
-              <option>...</option>
-            </Form.Select>
-          </Form.Group>
-          <InputGroup as={Col} controlId="tayttopaine">
-            <Form.Control type="number" defaultValue={0} />
-            <InputGroup.Text>bar</InputGroup.Text>
-          </InputGroup>
-          <Form.Group as={Col} controlId="lisatiedot">
-            <Form.Control placeholder="" />
-          </Form.Group>
-          <Form.Group as={Col} controlId="hinta">
-            <p>0,00 €</p>
-          </Form.Group>
-          <Form.Group as={Col} controlId="poista">
-            <Button className="removeRowButton">Poista rivi</Button>
-          </Form.Group>
-        </Row>
-        <Button className="addNewRowButton mt-5">Lisää uusi rivi</Button>
+      <Formik
+        initialValues={{}}
+        validate={(values) => {
+          const errors = {};
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <Form>
+            <LogBookFieldRow />
+            <LogBookRow />
 
-        <div className="mt-5">
-          <Button className="formButton">Tallenna</Button>
-          <Button className="formButton">Peruuta</Button>
-        </div>
-      </Form>
+            <Button className="addNewRowButton mt-5">Lisää uusi rivi</Button>
+
+            <div className="mt-5">
+              <Button className="formButton">Tallenna</Button>
+              <Button className="formButton">Peruuta</Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
