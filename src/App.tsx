e0c0490f6f9ce00cf5b6be4b9ab839609e724d5c
toppeh <38320587+toppeh@users.noreply.github.com>
2022-10-20 17:17:15 +0300

    User management view (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import { Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 import SignUp from './views/SignUp';
+import UserManagement from './views/UserManagement';
 import LogBook from './views/LogBook';
 import { Navbar } from './views/Navbar';
 
@@ -15,6 +16,7 @@ const App = (): JSX.Element => {
             <Route path="/" element={<Home />} />
             <Route path="register" element={<SignUp />} />
             <Route path="logbook" element={<LogBook />} />
+            <Route path="/management" element={<UserManagement />} />
           </Routes>
         </Container>
       </main>
