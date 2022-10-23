41c95e5c6beaa785499b90210c53444fc6f5696f
Akseli Kolari <akzuu@pm.me>
2022-10-23 16:12:32 +0300

    Removed unused code

--- src/components/DivingCylinderSet/newDivingCylinderSet.tsx
+++ src/components/DivingCylinderSet/newDivingCylinderSet.tsx
@@ -1,14 +1,4 @@
-// type DivingCylinderSet
-
 export const NewDivingCylinderSet = (): JSX.Element => {
-  // const [values, setValues] = useState();
-
-  // const handleSubmit = useCallback((event: React.FormEvent) => {
-  //   event.preventDefault();
-
-  //   // Validate
-  // }, []);
-
   return (
     <div>
       <h2>Uusi pullosetti</h2>
