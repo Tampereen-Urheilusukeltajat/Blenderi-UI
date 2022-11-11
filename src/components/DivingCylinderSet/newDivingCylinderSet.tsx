ac067b67a72608f683d44b995cf0a4be7d21f581
Akseli Kolari <akzuu@pm.me>
2022-11-11 08:38:42 +0200

    Base functionality for adding a new diving cylinder set

--- src/components/DivingCylinderSet/newDivingCylinderSet.tsx
+++ src/components/DivingCylinderSet/newDivingCylinderSet.tsx
@@ -1,8 +1,94 @@
+import { Field, FieldArray, FieldArrayRenderProps, Form, Formik } from 'formik';
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { BsTrash } from 'react-icons/bs';
+
+import '../../styles/divingCylinderSet/newDivingCylinderSet.css';
+import { IconButton } from '../common/IconButton';
+
+const NewDivingCylinderRow = (
+  { remove, push }: FieldArrayRenderProps,
+  index: number,
+  lastItem: boolean
+): JSX.Element => {
+  return (
+    <div key={index}>
+      <div className="flexRow">
+        <div className="labelAndInput">
+          <span>Koko (l)</span>
+          <Field name={`divingCylinders.${index}.volume`} />
+        </div>
+        <div className="labelAndInput">
+          <span>Materiaali</span>
+          <Field name={`divingCylinders.${index}.material`} />
+        </div>
+        <div className="labelAndInput">
+          <span>Suurin täyttöpaine (bar)</span>
+          <Field name={`divingCylinders.${index}.maximumFillingPressure`} />
+        </div>
+        <div className="labelAndInput">
+          <span>Sarjanumero</span>
+          <Field name={`divingCylinders.${index}.serialNumber`} />
+        </div>
+        <div className="labelAndInput">
+          <span>Katsastusvuosi</span>
+          <Field name={`divingCylinders.${index}.inspectionYear`} />
+        </div>
+        <IconButton
+          className="btn-danger deleteRowButton"
+          icon={<BsTrash />}
+          onClick={() => remove(index)}
+          disabled={lastItem && index === 0}
+        />
+      </div>
+      {lastItem ? (
+        <Button className="addNewDivingCylinder" onClick={() => push({})}>
+          Lisää pullo
+        </Button>
+      ) : null}
+    </div>
+  );
+};
+
 export const NewDivingCylinderSet = (): JSX.Element => {
   return (
     <div>
       <h2>Uusi pullosetti</h2>
-      <div></div>
+      <Formik
+        initialValues={{
+          divingCylinderSetName: '',
+          divingCylinders: [{}],
+        }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ values }) => (
+          <Form className="newCylinderSetForm">
+            <div className="flexRow">
+              <div className="labelAndInput">
+                <label htmlFor="divingCylinderSetName">Pullosetin nimi</label>
+                <Field
+                  id="divingCylinderSetName"
+                  name="divingCylinderSetName"
+                />
+              </div>
+              <Button type="submit">Tallenna pullosetti</Button>
+            </div>
+            <FieldArray name="divingCylinders">
+              {(arrayHelpers) => (
+                <>
+                  {values.divingCylinders.map((dc, index) =>
+                    NewDivingCylinderRow(
+                      arrayHelpers,
+                      index,
+                      values.divingCylinders.length === index + 1
+                    )
+                  )}
+                </>
+              )}
+            </FieldArray>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
