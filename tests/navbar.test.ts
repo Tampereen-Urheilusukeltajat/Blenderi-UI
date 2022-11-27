7ffa95b5d5a44929e5d6b90bffe4a9faf191f82b
Akseli Kolari <akzuu@pm.me>
2022-11-27 18:16:06 +0200

    Remove rest of the tests

--- tests/navbar.test.ts
+++ tests/navbar.test.ts
@@ -1,19 +1,17 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test.describe('Test navbar', () => {
-  test('navbar has logout button', async ({ page }) => {
-    await page.goto('/logbook');
-    // Click text=Kirjaudu ulos
-    await page.locator('button', { hasText: 'Kirjaudu ulos' }).click();
-  });
-
-  test('/ does not have navbar', async ({ page }) => {
-    await page.goto('/');
-    expect(await page.locator('.nav').isVisible()).toBe(false);
-  });
-
-  test('/register does not have navbar', async ({ page }) => {
-    await page.goto('/register');
-    expect(await page.locator('.nav').isVisible()).toBe(false);
-  });
+  // test('navbar has logout button', async ({ page }) => {
+  //   await page.goto('/logbook');
+  //   // Click text=Kirjaudu ulos
+  //   await page.locator('button', { hasText: 'Kirjaudu ulos' }).click();
+  // });
+  // test('/ does not have navbar', async ({ page }) => {
+  //   await page.goto('/');
+  //   expect(await page.locator('.nav').isVisible()).toBe(false);
+  // });
+  // test('/register does not have navbar', async ({ page }) => {
+  //   await page.goto('/register');
+  //   expect(await page.locator('.nav').isVisible()).toBe(false);
+  // });
 });
