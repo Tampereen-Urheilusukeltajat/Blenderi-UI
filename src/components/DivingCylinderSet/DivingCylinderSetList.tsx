47e2febde36681e779a593135fba91c8a0dd12d0
Akseli Kolari <33838340+Akzuu@users.noreply.github.com>
2023-01-23 13:54:31 +0200

    Refactoring & query and show users actual diving cylinders (#35)

--- src/components/DivingCylinderSet/DivingCylinderSetList.tsx
+++ src/components/DivingCylinderSet/DivingCylinderSetList.tsx
@@ -1,3 +1,7 @@
+import { useMemo } from 'react';
+import { DivingCylinderSet } from '../../lib/apiRequests/divingCylinderSetRequests';
+import { useDivingCylinderQuery } from '../../lib/queries/divingCylinderQuery';
+import { getUserIdFromAccessToken } from '../../lib/utils';
 import { CommonTable, TableColumn, TableRow } from '../common/Table';
 
 const DIVING_CYLINDER_SET_COLUMNS: TableColumn[] = [
@@ -21,34 +25,79 @@ const DIVING_CYLINDER_SET_COLUMNS: TableColumn[] = [
   },
 ];
 
-const TEMP_TEST_ROWS: TableRow[] = [
-  {
-    // Parser which creates this is going to be lit ! :)
-    mainRow: ['Mun D12', 24, null, null, null, null],
-    childRows: [
-      [null, 12, 'Teräs', 200, '11-22-33-44-55', '2016'],
-      [null, 12, 'Teräs', 200, '66-77-88-99-00', '2016'],
-    ],
-  },
-  {
-    mainRow: ['Mun sinkkupullo', 10, 'Teräs', 300, '55-22-33-55', '2020'],
-  },
-  {
-    mainRow: ['Mun toka sinkkupullo', 12, 'Teräs', 200, '55-42-33-55', '2013'],
-  },
-];
+export enum DivingCylinderMaterials {
+  steel = 'steel',
+  aluminium = 'aluminium',
+  carbonFiber = 'carbonFiber',
+}
+
+const DCMaterialFiTranslation = (enMaterial: string): string => {
+  switch (enMaterial) {
+    case DivingCylinderMaterials.aluminium:
+      return 'Alumiini';
+    case DivingCylinderMaterials.carbonFiber:
+      return 'Hiilikuitu';
+    case DivingCylinderMaterials.steel:
+      return 'Teräs';
+    default:
+      return enMaterial;
+  }
+};
+
+const divingCylinderSetsToCommonTableRows = (
+  divingCylinderSets: DivingCylinderSet[]
+): TableRow[] => {
+  const rows = divingCylinderSets.map((dcs) => {
+    // Single cylinder so only return mainRow
+    if (dcs.cylinders.length === 1) {
+      const dc = dcs.cylinders[0];
+      return {
+        mainRow: [
+          dcs.name,
+          dc.volume,
+          DCMaterialFiTranslation(dc.material),
+          dc.pressure,
+          dc.serialNumber,
+          new Date(dc.inspection).getFullYear(),
+        ],
+      };
+    }
+
+    const totalSetVolume = dcs.cylinders
+      .map((dc) => dc.volume)
+      .reduce((pv, cv) => (pv += cv));
+    return {
+      mainRow: [dcs.name, totalSetVolume, null, null, null, null],
+      childRows: [
+        ...dcs.cylinders.map((dc) => [
+          null,
+          dc.volume,
+          DCMaterialFiTranslation(dc.material),
+          dc.pressure,
+          dc.serialNumber,
+          new Date(dc.inspection).getFullYear(),
+        ]),
+      ],
+    };
+  });
+
+  return rows;
+};
 
 export const DivingCylinderSetList = (): JSX.Element => {
+  const userId = useMemo(() => getUserIdFromAccessToken(), []);
+
+  const divingCylinderSets = useDivingCylinderQuery(userId);
+  const rows = useMemo(
+    () => divingCylinderSetsToCommonTableRows(divingCylinderSets.data ?? []),
+    [divingCylinderSets]
+  );
+
   return (
     <div>
       <h1>Omat pullot</h1>
       <div>
-        <CommonTable
-          columns={DIVING_CYLINDER_SET_COLUMNS}
-          rows={TEMP_TEST_ROWS}
-          includeDeleteButton
-          includeEditButton
-        />
+        <CommonTable columns={DIVING_CYLINDER_SET_COLUMNS} rows={rows} />
       </div>
     </div>
   );
