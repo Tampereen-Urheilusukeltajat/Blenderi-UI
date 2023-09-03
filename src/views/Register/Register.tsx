b406343ddba27c054847c64a79f84e90eb45cb3a
Akseli Kolari <akzuu@pm.me>
2023-09-03 11:16:46 +0300

    Make password reset page responsive and add back button to the reset page

--- src/views/Register/Register.tsx
+++ src/views/Register/Register.tsx
@@ -1,17 +1,10 @@
-import { BsArrowLeftCircle } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
 import { RegisterForm } from '../../components/Register/RegisterForm';
 import styles from './Register.module.scss';
 
 export const Register: React.FC = () => {
   return (
-    <>
-      <Link className={styles.styledLink} to={'/'}>
-        <BsArrowLeftCircle />
-      </Link>
-      <div className={styles.content}>
-        <RegisterForm />
-      </div>
-    </>
+    <div className={styles.content}>
+      <RegisterForm />
+    </div>
   );
 };
