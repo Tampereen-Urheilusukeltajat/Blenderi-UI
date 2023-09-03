beb474a60a6293a094fd270c3afaacc5908fc83e
Akseli Kolari <akzuu@pm.me>
2023-09-03 11:24:31 +0300

    Migrate footer styles to css modules

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import '../../styles/footer/footer.css';
+import styles from './Footer.module.scss';
 import { ReactComponent as Tayttopaikka } from '../../svg/tayttopaikka.svg';
 
 export const Footer: React.FC = () => {
   return (
-    <div className="footer">
-      <div className="row">
-        <div className="logo">
+    <div className={styles.footer}>
+      <div className={styles.row}>
+        <div className={styles.logo}>
           <Tayttopaikka />
         </div>
       </div>
-      <div className="row">
-        <a href="https://taursu.fi" className="item">
+      <div className={styles.row}>
+        <a href="https://taursu.fi" className={styles.item}>
           Tampereen Urheilusukeltajat ry
         </a>
-        <a href="mailto:palaute@tayttopaikka.fi" className="item">
+        <a href="mailto:palaute@tayttopaikka.fi" className={styles.item}>
           Ota yhteyttä
         </a>
       </div>
-      <div className="row">
-        <a href="/gdpr" className="item">
+      <div className={styles.row}>
+        <a href="/gdpr" className={styles.item}>
           Tietosuojaseloste
         </a>
-        <a href="" aria-disabled="true" className="item">
+        <a href="" aria-disabled="true" className={styles.item}>
           Ohjeet
         </a>
       </div>
