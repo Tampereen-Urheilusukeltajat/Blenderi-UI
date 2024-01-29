18d595dbe3b46492207e0fc03352f57662be045e
Akseli Kolari <akzuu@pm.me>
2024-01-29 08:51:57 +0200

    Compressor queries

---
+++ src/lib/queries/compressorQuery.ts
@@ -0,0 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+import { UseQuery } from './common';
+import { COMPRESSOR_QUERY_KEY } from './queryKeys';
+import { getCompressors } from '../apiRequests/compressor';
+
+export type Compressor = {
+  id: string;
+  name: string;
+  description: string;
+  isEnabled: boolean;
+  airOnly: boolean;
+};
+
+export const useCompressorQuery = (): UseQuery<Compressor[]> => {
+  const { isLoading, data, isError } = useQuery({
+    queryKey: COMPRESSOR_QUERY_KEY,
+    queryFn: async () => getCompressors(),
+    onError: () => {
+      toast.error('Kompuroiden hakeminen epäonnistui. Yritä uudelleen.');
+    },
+    staleTime: 1000 * 60 * 60, // One hour
+    retry: 1,
+  });
+
+  return {
+    data,
+    isLoading,
+    isError,
+  };
+};
